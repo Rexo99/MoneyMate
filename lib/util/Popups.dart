ec068b74b352af3d04e9bdb49e5efb1c162483b1
Dorian Joshua Zimmermann <dorian-joshua.zimmermann@stud.hs-hannover.de>
2023-05-25 17:22:32 +0200

    oop

--- lib/util/Popups.dart
+++ lib/util/Popups.dart
@@ -112,28 +112,38 @@ void colorPicker({required Color currentColor, required ThemeMode currentThemeMo
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                  FilledButton(
-                      onPressed: () => MyApp.of(context).changeThemeColor(_colors[0]),
-                      child: Text('Red', textScaleFactor: .9), style: FilledButton.styleFrom(backgroundColor: _colors[0], shape: CircleBorder())),
-                  FilledButton(
-                      onPressed: () => MyApp.of(context).changeThemeColor(_colors[1]),
-                      child: Text('Purple', textScaleFactor: .9), style: FilledButton.styleFrom(backgroundColor: _colors[1], shape: CircleBorder())),
-                  FilledButton(
-                      onPressed: () => MyApp.of(context).changeThemeColor(_colors[2]),
-                      child: Text('Blue', textScaleFactor: .9), style: FilledButton.styleFrom(backgroundColor: _colors[2], shape: CircleBorder())),
+                    new Flexible(
+                        child: FilledButton(
+                            onPressed: () => MyApp.of(context).changeThemeColor(_colors[0]),
+                            child: Text('Red', textScaleFactor: .9), style: FilledButton.styleFrom(backgroundColor: _colors[0], shape: CircleBorder()))),
+                    new Flexible(
+                        child: FilledButton(
+                            onPressed: () => MyApp.of(context).changeThemeColor(_colors[1]),
+                            child: Text('Purple', textScaleFactor: .9), style: FilledButton.styleFrom(backgroundColor: _colors[1], shape: CircleBorder()))
+                    ),
+                    new Flexible(
+                        child: FilledButton(
+                            onPressed: () => MyApp.of(context).changeThemeColor(_colors[2]),
+                            child: Text('Blue', textScaleFactor: .9), style: FilledButton.styleFrom(backgroundColor: _colors[2], shape: CircleBorder()))
+                    ),
                   ],
                 ),
                 SizedBox(height: 10),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                  FilledButton(
-                      onPressed: () => MyApp.of(context).changeThemeColor(_colors[3]),
-                      child: Text('Green', textScaleFactor: .9), style: FilledButton.styleFrom(backgroundColor: _colors[3], shape: CircleBorder())),
-                  FilledButton(onPressed: () => MyApp.of(context).changeThemeColor(_colors[4]),
-                      child: Text('Yellow', textScaleFactor: .9), style: FilledButton.styleFrom(backgroundColor: _colors[4], shape: CircleBorder())),
-                  FilledButton(onPressed: () => MyApp.of(context).changeThemeColor(_colors[5]),
-                      child: Text('Brown', textScaleFactor: .9), style: FilledButton.styleFrom(backgroundColor: _colors[5], shape: CircleBorder())),
+                    new Flexible(
+                        child: FilledButton(
+                            onPressed: () => MyApp.of(context).changeThemeColor(_colors[3]),
+                            child: Text('Green', textScaleFactor: .9), style: FilledButton.styleFrom(backgroundColor: _colors[3], shape: CircleBorder()))),
+                    new Flexible(
+                        child: FilledButton(
+                            onPressed: () => MyApp.of(context).changeThemeColor(_colors[4]),
+                            child: Text('Yellow', textScaleFactor: .9), style: FilledButton.styleFrom(backgroundColor: _colors[4], shape: CircleBorder()))),
+                    new Flexible(
+                        child: FilledButton(
+                            onPressed: () => MyApp.of(context).changeThemeColor(_colors[5]),
+                            child: Text('Brown', textScaleFactor: .9), style: FilledButton.styleFrom(backgroundColor: _colors[5], shape: CircleBorder()))),
                   ],
                 ),
                 SizedBox(height: 25),
@@ -142,25 +152,27 @@ void colorPicker({required Color currentColor, required ThemeMode currentThemeMo
                 Text('Choose your Theme Mode', textAlign: TextAlign.center),
                 SizedBox(height: 10),
                 Center(
-                    child: ToggleButtons(
-                      children: [Icon(Icons.light_mode), Icon(Icons.dark_mode), Icon(Icons.app_shortcut)],
-                      isSelected: _selection,
-                      borderRadius: const BorderRadius.all(Radius.circular(8)),
-                      onPressed: (int index) {
-                          // The button that is tapped is set to true, and the others to false.
-                          for (int i = 0; i < _selection.length; i++) {
-                            _selection[i] = i == index;
-                          }
-                          switch(index){
-                            case 0: MyApp.of(context).changeTheme(ThemeMode.light);
-                            break;
-                            case 1: MyApp.of(context).changeTheme(ThemeMode.dark);
-                            break;
-                            case 2: MyApp.of(context).changeTheme(ThemeMode.system);
-                            break;
-                          }
-                      },
-                    )
+                    child:
+                    new Flexible(
+                        child: ToggleButtons(
+                          children: [Icon(Icons.light_mode), Icon(Icons.dark_mode), Icon(Icons.app_shortcut)],
+                          isSelected: _selection,
+                          borderRadius: const BorderRadius.all(Radius.circular(8)),
+                          onPressed: (int index) {
+                            // The button that is tapped is set to true, and the others to false.
+                            for (int i = 0; i < _selection.length; i++) {
+                              _selection[i] = i == index;
+                            }
+                            switch(index){
+                              case 0: MyApp.of(context).changeTheme(ThemeMode.light);
+                              break;
+                              case 1: MyApp.of(context).changeTheme(ThemeMode.dark);
+                              break;
+                              case 2: MyApp.of(context).changeTheme(ThemeMode.system);
+                              break;
+                            }
+                          },
+                    ))
                 ),
                 SizedBox(height: 25),
             ]),
