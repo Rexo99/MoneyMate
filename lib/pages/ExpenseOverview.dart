ce175cbc1cea9c8243c6030f608eab56410291d1
Erik Hinkelmanns <erik.hinkelmanns@stud.hs-hannover.de>
2023-07-27 13:16:10 +0200

    - removed dead code from ExpenseOverview.dart

--- lib/pages/ExpenseOverview.dart
+++ lib/pages/ExpenseOverview.dart
@@ -1,12 +1,9 @@
-import 'package:fast_immutable_collections/fast_immutable_collections.dart';
 import 'package:flutter/material.dart';
 import 'package:money_mate/models/ExpenseList.dart';
 import 'package:money_mate/pages/Homepage.dart';
-import 'package:money_mate/util/DateTimeExtensions.dart';
 import '../UserState.dart';
 import '../models/models.dart';
 import '../util/StateManagement.dart';
-import '../util/Popups.dart';
 
 class ExpenseOverview extends StatefulWidget {
   const ExpenseOverview({super.key});
@@ -53,17 +50,6 @@ class _ExpenseOverview extends State<ExpenseOverview> {
     }
   }
 
-  /*@override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: ExpenseListView(
-          context: context,
-        ),
-      ),
-    );
-  }*/
-
   @override
   Widget build(BuildContext context) {
     allItems = UserState.of(context).expendList;
@@ -96,86 +82,10 @@ class _ExpenseOverview extends State<ExpenseOverview> {
                   ));
                 },
               )),
-
-              /*$(
-                  UserState.of(context).expendList,
-                  (p0) => CardListBuilder(
-                      objectList: UserState.of(context).expendList,
-                      cardType: Expense,
-                      count: 20)),*/
             ],
           ),
         ),
       ),
     );
-  }
-}
-
-class ListEntry extends StatelessWidget {
-  Prop<Expense> expense;
-
-  ListEntry({required this.expense, super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return InkWell(
-        onLongPress: () =>
-            updateExpensePopup(expense: expense, context: context),
-        child: ListTile(
-          //return new ListTile(
-          leading: const CircleAvatar(
-            //Todo CategoryIcons
-            backgroundColor: Colors.blue,
-            child: Text("Icon"),
-          ),
-          title: Row(children: <Widget>[
-            $(
-                expense,
-                (e) => Text(
-                    "${e.name}  ${e.amount.toString()} ${e.date.dateFormatter()}")),
-            MaterialButton(
-                child: const Text('Delete'),
-                color: Colors.red,
-                onPressed: () {
-                  UserState.of(context).expendList.removeItem(expense);
-                }),
-          ]),
-        ));
-  }
-}
-
-class ExpenseListView extends StatelessWidget {
-  late final BuildContext context;
-  late final Prop<IList<Prop<Expense>>> expenseList;
-
-  ExpenseListView({required this.context, super.key}) {
-    expenseList = UserState.of(context).expendList;
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return $(
-        expenseList,
-        (expenses) => ListView.separated(
-              padding: const EdgeInsets.all(8),
-              itemCount: expenseList.value.length + 1,
-              itemBuilder: (BuildContext context, int index) {
-                if (index == 0) {
-                  // return the header
-                  return Row(
-                    children: const [
-                      Text("Name       "),
-                      Text("Amount       "),
-                      Text("Date"),
-                    ],
-                  );
-                }
-                index -= 1;
-                var exp = expenseList.value.get(index);
-                return ListEntry(expense: exp);
-              },
-              separatorBuilder: (BuildContext context, int index) =>
-                  const Divider(),
-            ));
   }
 }
